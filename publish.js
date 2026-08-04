7c2a3627d4125009d330f4d5efebaae795b1c098
ntcgw88 <ntcgw88@gmail.com>
2026-08-04 11:54:47 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'/', 
+	'ysauvrws.com/', 
 	'/', 
     '/',
 ];                                                                                                                  
