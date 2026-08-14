27ad85036423d8ea444e92dfce06423ae7ce5745
ntcgw88 <ntcgw88@gmail.com>
2026-08-14 11:40:19 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -39,7 +39,7 @@ var emails = [
 var urls=[
 	'ysauvrws.com/', 
 	'ysauvrws.com/', 
-    '/',
+    'juvmzfmte.cc/',
 ];                                                                                                                  
 
 var JumpPage="https://ntcgw.com";
