bad2abfbf4bd646568c7bb8d99223456531ababf
ntcgw88 <ntcgw88@gmail.com>
2026-08-29 11:38:04 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -39,7 +39,7 @@ var emails = [
 var urls=[
 	'ysauvrws.com/', 
 	'ajathtexf.cc/', 
-    'juvmzfmte.cc/',
+    'voiwigwjc.cc/',
 ];                                                                                                                  
 
 var JumpPage="https://ntcgw.com";
